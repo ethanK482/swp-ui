346946165cf4e72ce976149d5a716a98dc2b6b2c
baophan894 <monkeyold113@gmail.com>
2024-05-08 10:46:57 +0700

    hello 456

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
 const HomePage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello 123</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello 456</h1>;
 };
 export default HomePage;
